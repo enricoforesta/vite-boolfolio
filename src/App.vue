fad6ba746cd7bb16ba11d3125bd52f28148d3474
enricoforesta <enrico.foresta97@gmail.com>
2024-02-15 16:05:23 +0100

    feat: milestone 3 chiama axios

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <script>
+import axios from 'axios';
 import AppContent from './components/AppContent.vue';
 export default {
   name: 'App',
+  data() {
+    return {
+      Url: 'http://127.0.0.1:8000',
+      apiUrls: {
+        projects: '/api/project'
+      }
+    }
+  },
   components: {
     AppContent
+  },
+  methods: {
+    getProjects() {
+      axios.get(this.Url + this.apiUrls.projects).then(response => {
+        console.log(response)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+  },
+  created() {
+    this.getProjects();
   }
 }
 </script>
